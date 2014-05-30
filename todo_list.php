048946f5c5c8998edc2653c3ddacc528c9ea0296
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-30 11:46:13 -0500

    created a todo list in PHP to run on web w/ HTML forms. allows user to add task and remove task

---
+++ todo_list.php
@@ -0,0 +1,69 @@
+<?php
+define('FILENAME', 'data/list.txt');
+
+
+//  open a file process
+function open_file($filename) { 
+    $handle = fopen($filename, 'r'); // opens file and makes sure its readable
+    $contents = fread($handle, filesize($filename)); //checks file size
+    fclose($handle); // closes the file
+    return explode("\n", $contents); // breaks up the 'string' into a array
+}
+
+// 
+function save_file($filename, $array) {
+		if (is_writable($filename)) {     // checks to see is file is a writable file
+			$handle = fopen($filename, 'w'); // writes newly added task the the files
+			fwrite($handle, implode("\n", $array));  // takes array w/ new added task then converst to string
+			fclose($handle); // closes the file
+		}
+}
+
+$new_task = open_file(FILENAME);
+ 
+// if get ID in URL   
+if (isset($_GET['id'])) {
+	unset($new_task[$_GET['id']]); //delete item selected
+	save_file(FILENAME,$new_task); //saved changes 
+	$new_task = open_file(FILENAME); //reopen new file w/ changes
+}	
+
+if (isset($_POST['add_task'])) {
+	$item = trim($_POST['add_task']);
+	array_push($new_task, $item);
+	save_file(FILENAME,$new_task);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Codeup Todo list</title>
+	</head>
+	<body>
+		
+		<h1>TODO List</h1>
+		<ul>
+				<?php
+				foreach ($new_task as $key => $item){
+					echo "<li>$item<a href = '?id=$key'> -Remove task-</a></li>";				
+				}
+				
+				?>
+
+		</ul>
+			<h3>Do you need to add a task to your TODO list?<br>Simply type your task in box below and click ADD task:</h3>
+	        <form method="POST" action="todo_list.php">
+	            <p>
+	              <label for="add_task">Add item to TODO list:</label>
+	              <input id="add_task" name="add_task" type="text"placeholder="type task here">
+	            </p> 
+	            <p>
+	              <input type="submit">
+	            </p>  
+			</form>
+
+	</body>
+</html>
